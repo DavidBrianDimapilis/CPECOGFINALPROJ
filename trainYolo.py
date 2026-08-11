d02f483e514238ba53f89855da431a887d1f7455
Johnn <johnn_patrick_que@dlsu.edu.ph>
2026-08-11 12:01:39 +0800

    2nd run 100 epochs, 640 image size

--- trainYolo.py
+++ trainYolo.py
@@ -4,9 +4,9 @@ model = YOLO("yolo26n.pt")
 
 results = model.train(
 	data = "data.yaml",
-	epochs = 10,
-	imgsz = 320,
-	batch = 4,
-	name = "01_firstRun"
+	epochs = 100,
+	imgsz = 640,
+	batch = 16,
+	name = "02_realRun"
 
 		)
