aaec0701ff297d49c045688ff9e5ba21bc28abaa
Johnn <johnn_patrick_que@dlsu.edu.ph>
2026-08-11 12:11:37 +0800

    Please change the name the next time you train

--- trainYolo.py
+++ trainYolo.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO("yolo26n.pt")
 
+# NOTE: Please rename name to 03_xxxxx, 04_xxxxxxx
 results = model.train(
 	data = "data.yaml",
 	epochs = 100,
@@ -10,3 +11,4 @@ results = model.train(
 	name = "02_realRun"
 
 		)
+
