58af9f1857ce75815db4cf43396f5eb7d82215ce
DavidBrianDimapilis <131399581+DavidBrianDimapilis@users.noreply.github.com>
2026-08-14 03:10:36 +0800

    test

--- testYolo.py
+++ testYolo.py
@@ -1,29 +1,44 @@
 from ultralytics import YOLO
 
-MODEL_PATH = "models/03_90_epochs/best.pt"   # CHANGE 02_realRun to other folder names
-DATA_YAML = "data.yaml"          
+MODEL_PATH = "runs/detect/m4_median-4/weights/best.pt"
+DATA_YAML = "data.yaml"
 
-model = YOLO(MODEL_PATH)
 
-print("\n TESTING against annotations ")
-metrics = model.val(data=DATA_YAML, split="test", name="test_eval")
+if __name__ == "__main__":
 
-print("\n--- Test set results ---")
-print(f"mAP50-95: {metrics.box.map:.4f}")
-print(f"mAP50:    {metrics.box.map50:.4f}")
-print(f"Precision:{metrics.box.mp:.4f}")
-print(f"Recall:   {metrics.box.mr:.4f}")
-print("\nPer-class mAP50-95:")
-for i, class_name in model.names.items():
-    print(f"  {class_name}: {metrics.box.maps[i]:.4f}")
+    model = YOLO(MODEL_PATH)
 
-print("\n PREDICTING raw images\n")
-results = model.predict(
-    source="test/images",   
-    save=True,
-    conf=0.25,
-    name="test_predictions"
-)
+    print("\nTESTING against annotations")
 
-print(f"\nDone. Annotated test images saved in: runs/detect/test_predictions/")
-print(f"Test metrics/plots saved in: runs/detect/test_eval/")
+    metrics = model.val(
+        data=DATA_YAML,
+        split="test",
+        name="test_eval",
+        workers=0
+    )
+
+    print("\n--- Test set results ---")
+    print(f"mAP50-95: {metrics.box.map:.4f}")
+    print(f"mAP50:    {metrics.box.map50:.4f}")
+    print(f"Precision:{metrics.box.mp:.4f}")
+    print(f"Recall:   {metrics.box.mr:.4f}")
+
+    print("\nPer-class mAP50-95:")
+    for i, class_name in model.names.items():
+        print(f"  {class_name}: {metrics.box.maps[i]:.4f}")
+
+    print("\nPREDICTING raw images\n")
+
+    results = model.predict(
+        source="test/images",
+        save=True,
+        conf=0.25,
+        name="test_predictions",
+        workers=0
+    )
+
+    print("\nDone. Annotated test images saved in:")
+    print("runs/detect/test_predictions/")
+
+    print("\nTest metrics/plots saved in:")
+    print("runs/detect/test_eval/")
