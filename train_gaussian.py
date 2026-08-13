7d4d4617bb54e28dc757e7353c71a432104dc179
DavidBrianDimapilis <131399581+DavidBrianDimapilis@users.noreply.github.com>
2026-08-14 00:56:00 +0800

    fixed

---
+++ train_gaussian.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import os
+from ultralytics import YOLO
+
+def preprocess_dataset():
+    INPUT_FOLDER = "train/images"
+    OUTPUT_FOLDER = "train/preproc"
+    
+    print("Starting preprocessing...")
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    
+    for filename in os.listdir(INPUT_FOLDER):
+        if not filename.lower().endswith((".jpg", ".jpeg", ".png")):
+            continue
+
+        imagePath = os.path.join(INPUT_FOLDER, filename)
+        image = cv2.imread(imagePath)
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+        
+        outputPath = os.path.join(OUTPUT_FOLDER, filename)
+        cv2.imwrite(outputPath, blurred)
+
+    print("Preprocessed inside ", OUTPUT_FOLDER)
+
+if _name_ == '_main_':
+    preprocess_dataset()
+    
+    model = YOLO("yolo26n.pt") 
+    
+    results = model.train(
+        data="data.yaml", 
+        epochs=150,
+        imgsz=640,
+        batch=-1,
+        patience=15,
+        name="04_150_epochs_15_patience"
+    )
